d33a1ea28a724e88cdb108400ab6daf9438babb9
ryan <texasryan82@gmail.com>
2019-04-06 15:17:50 -0500

    created tests for create and insert

---
+++ trie.go
@@ -0,0 +1,24 @@
+package trie
+
+// Trie is an implementation of a prefix tree used for storing and searching strings
+type Trie interface {
+	Count() int
+	Insert(word string)
+}
+
+type trie struct {
+	count int
+}
+
+// NewTrie initializes the Trie
+func NewTrie() Trie {
+	return &trie{}
+}
+
+func (t *trie) Count() int {
+	return t.count
+}
+
+func (t *trie) Insert(word string) {
+	t.count++
+}
---
+++ trie_test.go
@@ -0,0 +1,24 @@
+package trie
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+	trie := NewTrie()
+
+	if trie == nil {
+		t.Error("trie cannot be nil")
+	}
+
+	if trie.Count() != 0 {
+		t.Error("trie should have zero words after creation")
+	}
+}
+
+func TestInsertWord(t *testing.T) {
+	trie := NewTrie()
+
+	trie.Insert("foobar")
+	if trie.Count() != 1 {
+		t.Error("trie should have one word")
+	}
+}
