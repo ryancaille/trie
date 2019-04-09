1ee666d913c0578e67eb388a1e75f2a42dc9a2b9
ryan <texasryan82@gmail.com>
2019-04-08 21:26:23 -0500

    refactored node.go to use a single search method

--- node.go
+++ node.go
@@ -30,50 +30,57 @@ func newNode(r rune, suffix []rune, parent *node) (*node, bool) {
 }
 
 func insert(nodes []*node, word []rune, parent *node) ([]*node, bool) {
+
 	var inserted bool
 
-	r := word[0]
-	suffix := word[1:]
+	prefix, suffix := word[0], word[1:]
 
-	if len(nodes) == 0 {
-		node, _ := newNode(r, suffix, parent)
-		nodes = append(nodes, node)
-		inserted = true
-	} else {
-		// TODO: create Search method to combine with search from contains
-		index := sort.Search(len(nodes), func(i int) bool { return nodes[i].value >= r })
-		if index >= 0 && index < len(nodes) && nodes[index].value == r {
-			if len(suffix) > 0 {
-				nodes[index].children, inserted = insert(nodes[index].children, suffix, nodes[index])
-			} else {
-				nodes[index].endOfWord = true
-				inserted = true
-			}
+	_, node := search(nodes, prefix)
+	if node != nil {
+		if len(suffix) > 0 {
+			node.children, inserted = insert(node.children, suffix, node)
+		} else {
+			inserted, node.endOfWord = true, true
 		}
+	} else {
+
+		node, _ := newNode(prefix, suffix, parent)
+
+		// TODO: We have to keep the children sorted or the Binary search won't work!!
+		// use the index of the search method to insert the node
+		nodes = append(nodes, node)
+
+		inserted = true
 	}
 
 	return nodes, inserted
 }
 
+// contains recursively searches children until it reaches the end of the word,
+// or it cannot find a node matching the rune
 func contains(nodes []*node, word []rune) bool {
 
-	r := word[0]
-	endOfWord := len(word) == 1
-
-	// TODO: create Search method to combine with search from insert
-	index := sort.Search(len(nodes), func(i int) bool { return nodes[i].value >= r })
-	if index >= 0 && index < len(nodes) && nodes[index].value == r {
-
-		if endOfWord && nodes[index].endOfWord {
-			return true
-		}
-
-		if endOfWord && !nodes[index].endOfWord {
-			return false
-		}
-
-		return contains(nodes[index].children, word[1:])
+	// Search for the node that matches the rune
+	_, node := search(nodes, word[0])
+	if node == nil {
+		return false
 	}
 
-	return false
+	// if this is the last rune in the word, we don't search anymore children
+	if endOfWord := len(word) == 1; endOfWord {
+		return node.endOfWord
+	}
+
+	// recursively search the children
+	return contains(node.children, word[1:])
+}
+
+// search looks for the node where the value matches the rune
+func search(nodes []*node, r rune) (int, *node) {
+	index := sort.Search(len(nodes), func(i int) bool { return nodes[i].value >= r })
+	if index >= 0 && index < len(nodes) && nodes[index].value == r {
+		return index, nodes[index]
+	}
+
+	return index, nil
 }
